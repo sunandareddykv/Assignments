f3ce404e43228176736f20fcc7894aa807f189a0
sunandareddykv <sunandareddy.k@gmail.com>
2026-01-09 11:36:35 +0530

    nodescript-training added

---
+++ nodescript-training/datatypes-basics/variable-declaration-datatypes.ts
@@ -0,0 +1,25 @@
+//Initialization
+const a:number = 10;
+let b:number = 20;
+var c:number = 30;
+
+//Re-assignment
+
+
+//Re-Declaration
+
+
+
+//Scope
+
+{
+    const a:number = 10;
+    let b:number = 20;
+    var c:number = 30;
+    console.log(a);
+    console.log(b);
+    console.log(c);
+}
+console.log(a);
+console.log(b);
+console.log(c);
